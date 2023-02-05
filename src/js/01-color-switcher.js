5fd1c52bf8f1e749a5731f83a4a27261cb74a6a9
Andrii Safonov <105196765+AndriySafonov@users.noreply.github.com>
2023-02-05 12:12:17 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
 
+const startBtn = document.getElementById('data-start');
+const stopBtn = document.getElementById('data-stop');
+let timer = null;
+
+startBtn.addEventListener("click", onStart);
+stopBtn.addEventListener("click", onStop);
+
+function onStart() {
+    timer = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    },1000);
+    startBtn.disabled = true;
+    
+}
+
+function onStop() {
+    clearInterval(timer);
+    stopBtn.disabled = false;
+    
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
