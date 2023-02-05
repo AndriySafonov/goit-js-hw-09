4a9c39a1ac287c0d2b51b657b633438ed205df40
Andrii Safonov <105196765+AndriySafonov@users.noreply.github.com>
2023-02-05 18:31:33 +0200

    task 2 finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,7 +2,19 @@ function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    resolve('fulfill')
   } else {
     // Reject
+    reject('rejected')
   }
 }
+
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  console.log(createPromise(2, 1500));
