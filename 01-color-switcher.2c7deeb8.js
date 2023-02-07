197a9eda2d411cc0be08f413e950c31929d8f16f
AndriySafonov <105196765+AndriySafonov@users.noreply.github.com>
2023-02-07 12:44:18 +0000

    Deploying to gh-pages from @ AndriySafonov/goit-js-hw-09@e7f2d60bf7259322d7a4fef76998b037a3e011e3 🚀

---
+++ 01-color-switcher.2c7deeb8.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let o=null;e.addEventListener("click",(function(){o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(o),t.disabled=!0,e.disabled=!1,console.log("stop")}));
+//# sourceMappingURL=01-color-switcher.2c7deeb8.js.map
